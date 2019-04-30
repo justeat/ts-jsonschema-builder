a5b8047ba3a79e9aec103b1c9029a0a33ce5cb1d
Sergej <sergejpopov@outlook.com>
2019-04-30 16:24:51 +0100

    Fix exclusive max/min for floating numbers (#1)

--- src/expression-parser.ts
+++ src/expression-parser.ts
@@ -2,7 +2,8 @@ import * as esprima from "esprima";
 import { ExpressionStatement, ArrowFunctionExpression, ReturnStatement, Literal } from "estree";
 
 
-export function parseAsRange(expression: Function): { min: number, max: number } {
+export type range = { min?: number, max?: number, isMaxExclusive?: boolean, isMinExclusive?: boolean };
+export function parseAsRange(expression: Function): range {
 
   const expr = ((
     esprima.parseModule(expression.toString().replace(/function \(?(\w)\)?/, "$1 =>"))
@@ -18,12 +19,18 @@ export function parseAsRange(expression: Function): { min: number, max: number }
 
   const val = (rangeExpression.right as Literal).value as number;
 
-  const range = {} as any;
+  const range: range = {};
 
   if (["==", "===", ">="].includes(rangeExpression.operator)) { range.min = val; }
   if (["==", "===", "<="].includes(rangeExpression.operator)) { range.max = val; }
-  if (rangeExpression.operator === "<") { range.max = val - 1; }
-  if (rangeExpression.operator === ">") { range.min = val + 1; }
+  if (rangeExpression.operator === "<") {
+    range.max = val;
+    range.isMaxExclusive = true;
+  }
+  if (rangeExpression.operator === ">") {
+    range.min = val;
+    range.isMinExclusive = true;
+  }
 
   return range;
 }
